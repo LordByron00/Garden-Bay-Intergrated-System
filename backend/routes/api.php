9225cbe25e58977d93fb8e7bb2ceee7b0e0414c2
Lord Byron <Jbayron76@gmail.com>
2025-05-20 17:00:29 +0800

    Adding Garden-Bay-Ordering-System and Garden-Bay-Backend as submodules

--- backend/routes/api.php
+++
@@ -1,8 +0,0 @@
-<?php
-use Illuminate\Support\Facades\Route;
-use  App\Http\Controllers\MenuItemController;
-use  App\Http\Controllers\OrderController;
-
-
-Route::get('/menu-items', [MenuItemController::class, 'index']);
-Route::post('/orders', [OrderController::class, 'store']);
